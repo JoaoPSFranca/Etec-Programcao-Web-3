2b7a7f981df0283d012e80078f5ad31855d81a07
Joao Pedro <francasjoaopedro@gmail.com>
2024-12-02 15:20:53 -0300

    add: Lamapada project
    Excersise project C# .Net

---
+++ prj35347_Lampada/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace prj35347_Lampada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsLampada lampada = new clsLampada();
+            string conexao = "";
+            int cont = 0;
+            bool status = true;
+
+            while (true)
+            {
+                conexao = "";
+                cont = 0;
+                status = true;
+
+                while (conexao.ToLower().Trim() != "s")
+                {
+                    Console.WriteLine("Nenhuma lâmpada encontrada! ");
+                    Console.WriteLine("Deseja colocar uma lâmpada? s/n ");
+                    conexao = Console.ReadLine().ToString();
+
+                    if (conexao.ToLower().Trim() == "s")
+                    {
+                        break;
+                    }
+
+                    if (lampada.VerificarLetraS(conexao.ToLower().Trim()))
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Nenhuma lâmpada encontrada! ");
+                        Console.WriteLine("Deseja colocar uma lâmpada? s/n ");
+                        conexao = Console.ReadLine().ToString();
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Informe apenas 's' ou 'n' ");
+                        Console.WriteLine();
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Lâmpada conectada! ");
+                Console.WriteLine("Pressione alguma tecla para ligar a lâmpada! ");
+                Console.ReadKey();
+
+                while (cont < 4)
+                {
+
+                    Console.WriteLine();
+                    Console.WriteLine(lampada.verificarStatus(status));
+                    Console.WriteLine(lampada.verificarStatus2(status));
+                    Console.ReadKey();
+                    status = !status;
+                    cont++;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("A lâmpada queimou! ");
+                Console.WriteLine();
+            }
+
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ prj35347_Lampada/clsLampada.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj35347_Lampada
+{
+    class clsLampada
+    {
+        public bool VerificarLetraS(string letra)
+        {
+            if (letra == "n")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }       
+
+        public string verificarStatus(bool status)
+        {
+            if (status)
+            {
+                return "Status: Lâmpada ligada! ";
+            }
+            else
+            {
+                return "Status: Lâmpada desligada! ";
+            }
+        }
+
+        public string verificarStatus2(bool status)
+        {
+            if (status)
+            {
+                return "Pressione alguma tecla para desligar a lâmpada. ";
+            }
+            else
+            {
+                return "Pressione alguma tecla para ligar a lâmpada. ";
+            }
+        }
+    }
+}
